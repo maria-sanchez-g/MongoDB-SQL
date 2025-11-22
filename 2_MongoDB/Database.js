a1cdc73e241e445e3753cf6d62e61f12504a8013
maria-sanchez-g <marysg_92@hotmail.com>
2025-11-22 13:24:21 +1100

    last

---
+++ 2_MongoDB/Database.js
@@ -0,0 +1,66 @@
+//Created a collection called Exercise_2
+
+//Created 4 Files:
+
+// Coments:
+// {
+//   "_id": {
+//     "$oid": "69210e7e6806d337f9f1e8ab"
+//   },
+//   "UserID": {
+//     "$oid": "676aaa0011bb22cc33dd44ee"
+//   },
+//   "PostID": {
+//     "$oid": "676bbb2233cc44dd55ee66ff"
+//   },
+//   "CreatedDate": {
+//     "$date": "2025-11-22T01:14:38.242Z"
+//   },
+//   "Content": "Beautiful photo!"
+// }
+
+// likes:
+
+// {
+//   "_id": {
+//     "$oid": "69210eee6806d337f9f1e8ac"
+//   },
+//   "UserID": {
+//     "$oid": "676aaa0011bb22cc33dd44ee"
+//   },
+//   "PostID": {
+//     "$oid": "676bbb2233cc44dd55ee66ff"
+//   },
+//   "CreatedDate": {
+//     "$date": "2025-11-22T01:16:30.399Z"
+//   }
+// }
+
+// //post:
+
+// {
+//   "_id": {
+//     "$oid": "69210e6a6806d337f9f1e8aa"
+//   },
+//   "ImageURL": "http://example.com/pic1.jpg",
+//   "Title": "My first post",
+//   "CreatedDate": {
+//     "$date": "2025-11-22T01:14:18.015Z"
+//   },
+//   "Description": "This is my first post",
+//   "UserID": {
+//     "$oid": "676aaa0011bb22cc33dd44ee"
+//   }
+// }
+
+// users:
+
+// {
+//   "_id": {
+//     "$oid": "69210a2c6806d337f9f1e8a9"
+//   },
+//   "FullName": "Edna Maria Romero",
+//   "Password": "12345",
+//   "Email": "edna@gmail.com",
+//   "UserName": "ednaromero"
+// }
